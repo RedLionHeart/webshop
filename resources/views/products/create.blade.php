c60eb52e24ef4f79de3e16a9cd7560906f56530b
Web <web@it.step>
2018-12-03 21:21:34 +0300

    first commit

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.layout')
+
+@section('content')
+    <table class="table table-striped">
+       {{-- <tr>
+            <td>Title</td>
+            <td>Content</td>
+            <td>Price</td>
+            <td>Delete</td>
+            <td>Update</td>
+        </tr>
+        @foreach($products as $product)
+            <tr>
+                <td>{{ $product['title'] }}</td>
+                <td>{{ $product['content'] }}</td>
+                <td>{{ $product['price'] }}</td>
+                <td><a href="" class="btn btn-danger">Delete</a></td>
+                <td><a href="" class="btn btn-success">Update</a></td>
+            </tr>
+        @endforeach--}}
+        <p>hello</p>
+    </table>
+@endsection
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layout')
+
+@section('content')
+    <table class="table table-striped">
+        <tr>
+            <td>Title</td>
+            <td>Content</td>
+            <td>Price</td>
+            <td>Delete</td>
+            <td>Update</td>
+        </tr>
+    @foreach($products as $product)
+        <tr>
+            <td>{{ $product['title'] }}</td>
+            <td>{{ $product['content'] }}</td>
+            <td>{{ $product['price'] }}</td>
+            <td><a href="" class="btn btn-danger">Delete</a></td>
+            <td><a href="" class="btn btn-success">Update</a></td>
+        </tr>
+    @endforeach
+    </table>
+@endsection
